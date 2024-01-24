85ee5c87853c119f8ead0257ba8a08c649a9669f
johaGL <johanna.galvis@ibgc.cnrs.fr>
2024-01-24 14:36:38 +0100

    update(main): add help menu, using click library

--- src/dimet/__main__.py
+++ src/dimet/__main__.py
@@ -8,10 +8,13 @@ import logging
 import os, sys
 
 import hydra
+
 from dimet.method import Method
 from omegaconf import DictConfig, OmegaConf
 
 from dimet.data import Dataset
+
+import click
 
 logger = logging.getLogger(__name__)
 
@@ -31,5 +34,24 @@ def main_run_analysis(cfg: DictConfig) -> None:
     method.run(cfg, dataset)
 
 
+@click.command(context_settings=dict(
+        help_option_names=['-h', '--help']  # needed to recognize -h as well
+    )
+)
+@click.option('-cd', help="Directory containing the general configuration"
+                          " .yaml files")
+@click.option('-cn', help="File name of the general configuration "
+                          "(only one .yaml file name)")
+def main_run_analysis__or_display_help(cd, cn) -> None:
+    if (cd is None) and (cn is None):
+        click.echo("This is DIMet, type --help or -h for help")
+    elif (cd is None) and (cn is not None):
+        click.echo("Please set the option -cd")
+    elif (cd is not None) and (cn is None):
+        click.echo("Please set the option -cn")
+    elif not ((cd is None) and (cn is None)):
+        main_run_analysis()  # run, hydra handles further arguments validation
+
+
 if __name__ == "__main__":
-    main_run_analysis()
+    main_run_analysis__or_display_help()
